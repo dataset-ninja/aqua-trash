e43ca8d2c9ad1dc8f619ea0f438ec715ee2454b4
peixed351 <peixed351@gmail.com>
2024-02-20 16:04:30 +0300

    add settings

--- src/convert.py
+++ src/convert.py
@@ -1,12 +1,16 @@
-import supervisely as sly
+import csv
 import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
-from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
 import shutil
+from collections import defaultdict
+from urllib.parse import unquote, urlparse
 
+import supervisely as sly
+from dataset_tools.convert import unpack_if_archive
+from supervisely.io.fs import get_file_name, get_file_name_with_ext
 from tqdm import tqdm
+
+import src.settings as s
+
 
 def download_dataset(teamfiles_dir: str) -> str:
     """Use it for large datasets to convert them on the instance"""
@@ -29,7 +33,7 @@ def download_dataset(teamfiles_dir: str) -> str:
             total=fsize,
             unit="B",
             unit_scale=True,
-        ) as pbar:        
+        ) as pbar:
             api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
         dataset_path = unpack_if_archive(local_path)
 
@@ -57,7 +61,8 @@ def download_dataset(teamfiles_dir: str) -> str:
 
         dataset_path = storage_dir
     return dataset_path
-    
+
+
 def count_files(path, extension):
     count = 0
     for root, dirs, files in os.walk(path):
@@ -65,21 +70,72 @@ def count_files(path, extension):
             if file.endswith(extension):
                 count += 1
     return count
-    
+
+
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
     ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    images_path = "/home/alex/DATASETS/TODO/AquaTrash/Images"
+    bboxes_path = "/home/alex/DATASETS/TODO/AquaTrash/annotations.csv"
+    batch_size = 30
+    ds_name = "ds"
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    def create_ann(image_path):
+        labels = []
 
-    # ... some code here ...
+        image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        img_height = image_np.shape[0]
+        img_wight = image_np.shape[1]
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+        ann_data = name_to_bboxes[get_file_name_with_ext(image_path)]
+        for curr_ann_data in ann_data:
+            obj_class_name = curr_ann_data[-1]
+            obj_class = meta.get_obj_class(obj_class_name)
+            coords = list(map(int, curr_ann_data[:4]))
+            left = int(coords[0])
+            top = int(coords[1])
+            right = int(coords[2])
+            bottom = int(coords[3])
+            rect = sly.Rectangle(left=left, top=top, right=right, bottom=bottom)
+            label = sly.Label(rect, obj_class)
+            labels.append(label)
 
-    # return project
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
 
+    glass = sly.ObjClass("glass", sly.Rectangle)
+    paper = sly.ObjClass("paper", sly.Rectangle)
+    metal = sly.ObjClass("metal", sly.Rectangle)
+    plastic = sly.ObjClass("plastic", sly.Rectangle)
 
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(obj_classes=[glass, paper, metal, plastic])
+    api.project.update_meta(project.id, meta.to_json())
+
+    name_to_bboxes = defaultdict(list)
+
+    with open(bboxes_path, "r") as file:
+        csvreader = csv.reader(file)
+        for row in csvreader:
+            name_to_bboxes[row[0]].append(row[1:])
+
+    dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+    images_names = os.listdir(images_path)
+
+    progress = sly.Progress("Create dataset {}".format(ds_name), len(images_names))
+
+    for images_names_batch in sly.batched(images_names, batch_size=batch_size):
+        img_pathes_batch = [
+            os.path.join(images_path, image_name) for image_name in images_names_batch
+        ]
+
+        img_infos = api.image.upload_paths(dataset.id, images_names_batch, img_pathes_batch)
+        img_ids = [im_info.id for im_info in img_infos]
+
+        anns = [create_ann(image_path) for image_path in img_pathes_batch]
+        api.annotation.upload_anns(img_ids, anns)
+
+        progress.iters_done_report(len(images_names_batch))
+
+    return project
--- src/settings.py
+++ src/settings.py
@@ -13,54 +13,89 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "Aqua Trash"
+PROJECT_NAME_FULL: str = "Aqua Trash Dataset"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.MIT(
+    source_url="https://github.com/Harsh9524/AquaTrash/blob/master/LICENSE"
+)
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Environmental()]
+CATEGORY: Category = Category.Environmental()
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
 RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
-    RELEASE_YEAR: int = None
+    RELEASE_YEAR: int = 2020
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "https://github.com/Harsh9524/AquaTrash"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 14101709
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/aqua-trash"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = "https://github.com/Harsh9524/AquaTrash"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
-CLASS2COLOR: Optional[Dict[str, List[str]]] = None
+CLASS2COLOR: Optional[Dict[str, List[str]]] = {
+    "glass": [230, 25, 75],
+    "paper": [60, 180, 75],
+    "metal": [255, 225, 25],
+    "plastic": [0, 130, 200],
+}
+
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
-PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
+PAPER: Optional[Union[str, List[str], Dict[str, str]]] = (
+    "https://www.sciencedirect.com/science/article/pii/S2666016420300244?via%3Dihub"
+)
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
-REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {"GitHub":"some_link_to_repo_if_exists"}
+REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
 CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
-AUTHORS_CONTACTS: Optional[List[str]] = None
+AUTHORS: Optional[List[str]] = [
+    "Harsh Panwar",
+    "P.K. Gupta",
+    "Mohammad Khubeb Siddiqui",
+    "Ruben Morales-Menendez",
+    "Prakhar Bhardwaj",
+    "Sudhansh Sharma",
+    "Iqbal H. Sarker",
+]
+AUTHORS_CONTACTS: Optional[List[str]] = [
+    "harshpanwar@ieee.org",
+    "pkgupta@ieee.org",
+    "khubeb@tec.mx",
+    "rmm@tec.mx",
+    "prakharbhardwaj784@gmail.com",
+    "sudhansh0706@gmail.com",
+    "iqbal.sarker.cse@gmail.com",
+]
 
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = [
+    "Department of Computer Science and Engineering, India",
+    "School of Engineering and Sciences, Mexico",
+    "Department of Computer Science and Engineering, Bangladesh",
+]
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = [
+    "https://www.cse.iitb.ac.in/",
+    "https://maestriasydiplomados.tec.mx/",
+    "https://buft.edu.bd/department-of-computer-science-and-engineering",
+]
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
 SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
